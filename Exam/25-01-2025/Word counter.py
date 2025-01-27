0392c8f9474a657efd63a2964e4d8713b4193af4
bobby0912 <bobby0912@gmail.com>
2025-01-27 09:26:17 +0530

    ..

--- Exam/25-01-2025/Word counter.py
+++ Exam/25-01-2025/Word counter.py
@@ -1,20 +1,29 @@
 def word_count(str):
-    # arr = str.split()
+    arr = str.split()
     # return len(arr)
-    c=0
-    for i in str:
-        if i==" ":
-            c+=1
-    # if last character is not space then increment count by 1
-    if str[-1]!=" ":
-        c+=1
-    return c
+    # c=0
+    # for i in str:
+    #     if i==" ":
+    #         c+=1
+    # # if last character is not space then increment count by 1
+    # if str[-1]!=" ":
+    #     c+=1
+    # return c
+    d={}
+    for i in arr:
+        d[i]=0
+    for i in arr:
+        d[i]+=1
+    for i in d:
+        print(i,d[i])
+
 
 def main():
     try:
         print("Enter text to check word count: ")
         str=input()
-        print("Word Count: ",word_count(str))
+        word_count(str)
+        # print("Word Count: ",word_count(str))
 
     except ValueError as e:
         print(f"Invalid Input {e}")
